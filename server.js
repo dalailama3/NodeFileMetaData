03a1ef2cb991116993af400065af87dca6e31eed
John Clark <johnclark@Johns-MacBook-Pro.local>
2016-12-31 20:07:51 -0500

    npm install multer for handling file data, add a form in index.html with a file input, sumbit will send a POST request to /file-upload, add app route for /file-upload and use multer to read the sent file

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var path = require('path')
+var multer = require('multer')
+var upload = multer( { dest: 'uploads'})
 var app = express()
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.post('/file-upload', upload.single('file'), function (req, res) {
+
+
+  res.json({
+    "bytes": req.file.size
+  })
 })
 
 app.listen(3000)
